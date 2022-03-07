85a3fe6bb1af27f26dc24ad4057fd117b3dd98fe
RicoWilliams <ricowilliams@protonmail.com>
2022-03-07 15:26:57 -0700

    ImageScreen component rendering images properly

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ImageDetail from '../Components/ImageDetail';
 
 const ImageScreen = () => {
-  return <Text>Image Screen</Text>;
+  return (
+    <View>
+      <ImageDetail
+        title="Forest"
+        imageSource={require('../../assets/forest.jpg')}
+        imageScore={10}
+      />
+      <ImageDetail
+        title="Beach"
+        imageSource={require('../../assets/beach.jpg')}
+        imageScore={5}
+      />
+      <ImageDetail
+        title="Mountain"
+        imageSource={require('../../assets/mountain.jpg')}
+        imageScore={2}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});

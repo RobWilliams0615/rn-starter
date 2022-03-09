89c7b067973381ade6a2b7a0a5cb1458e9beccfb
RicoWilliams <ricowilliams@protonmail.com>
2022-03-08 20:13:00 -0700

    reducer working on squarescreen

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -3,24 +3,53 @@ import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../Components/ColorCounter';
 
 const COLOR_INCREMENT = 25;
+
+const reducer = (state, action) => {
+  // state === {red: number, green: number, blue: number}
+  // action === {colorToChange: 'red' || 'green' || 'blue', amount: 25 || -25}
+  switch (action.colorToChange) {
+    case 'red':
+      return { ...state, red: state.red + action.amount };
+    case 'green':
+      return { ...state, green: state.green + action.amount };
+    case 'blue':
+      return { ...state, blue: state.blue + action.amount };
+    default:
+      return state;
+  }
+};
+
 const SquareScreen = () => {
-  const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0 })
-// console.log(state)
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+  const { red, green, blue } = state;
+
   return (
     <View>
       <ColorCounter
-        onIncrease={() => }
-        onDecrease={() => }
+        onIncrease={() =>
+          dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })
+        }
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => }
-        onDecrease={() => }
+        onIncrease={() =>
+          dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })
+        }
         color="Blue"
       />
       <ColorCounter
-        onIncrease={() => }
-        onDecrease={() => }
+        onIncrease={() =>
+          dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })
+        }
         color="Green"
       />
       <View

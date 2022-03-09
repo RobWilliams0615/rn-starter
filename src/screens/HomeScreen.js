bd3069fc421c9f80c6b551475451d41850d8d0bc
RicoWilliams <ricowilliams@protonmail.com>
2022-03-09 10:16:21 -0700

    TextScreen componene added, button added to homescreen, route added to app.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -30,6 +30,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Square Demo"
         onPress={() => navigation.navigate('Square')}
       />
+      <Button
+        title="Go to Text Input Demo"
+        onPress={() => navigation.navigate('Text')}
+      />
     </View>
   );
 };
---
+++ src/screens/TextScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Input, Button, StyleSheet } from 'react-native';
+
+const TextScreen = () => {
+  return (
+    <View>
+      <Text>Text Screen</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({});
+
+export default TextScreen;

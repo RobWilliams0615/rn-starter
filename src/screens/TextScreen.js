40aa8350ff819f411b248f2871cc54bb5d888b14
RicoWilliams <ricowilliams@protonmail.com>
2022-03-09 11:33:31 -0700

    input working on textscreen, conditiona rendering working properly

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import { View, Text, Input, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 const TextScreen = () => {
+  const [password, setPassword] = useState('');
+
   return (
     <View>
-      <Text>Text Screen</Text>
+      <Text>Enter Password:</Text>
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
+      />
+      {password.length < 4 ? (
+        <Text>Password muat be tleast 4 characters</Text>
+      ) : null}
     </View>
   );
 };
 
-const style = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
 
 export default TextScreen;
